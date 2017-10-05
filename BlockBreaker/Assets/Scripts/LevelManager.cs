8f9f079d6bb47c0cb7c4c72c5ed561ebc8e6ffd9
Unknown <jesswrightica@gmail.com>
2017-10-05 11:14:12 -0600

    Worked on Fugni Kingdom

--- BlockBreaker/Assets/Scripts/LevelManager.cs
+++ BlockBreaker/Assets/Scripts/LevelManager.cs
@@ -1,8 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour {
+
+
+	public void LevelLoad (string lvl) {
+		SceneManager.LoadScene (lvl);
+	}
 
 	public void Exit () {
 		print ("Tried to Exit.");
