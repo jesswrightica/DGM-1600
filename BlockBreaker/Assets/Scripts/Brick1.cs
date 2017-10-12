131c282913720549462b2624311fc63f9fadc536
Unknown <jesswrightica@gmail.com>
2017-10-12 11:24:29 -0600

    Made Bricks and Health

---
+++ BlockBreaker/Assets/Scripts/Brick1.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick1 : MonoBehaviour {
+
+	public int health;
+
+
+	void OnCollisionEnter (Collision collision) {
+
+
+	}
+		
+}
