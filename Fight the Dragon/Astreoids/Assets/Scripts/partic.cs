c5dfd675b4d75091070be6a599a3380ce5ca218a
Unknown <jesswrightica@gmail.com>
2017-11-14 11:40:38 -0700

    meterors

---
+++ Fight the Dragon/Astreoids/Assets/Scripts/partic.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class partic : MonoBehaviour {
+
+	public float particleHeight;
+
+	private Transform particleTrans;
+
+
+	// Use this for initialization
+	void Start () {
+		particleTrans = gameObject.GetComponent<Transform> ();
+	}
+
+	void Update () {
+		//get mouse position, translating to world coordinates
+		Vector3 mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		//overide vertical y position
+		mousePos.y = particleHeight;
+		//override depth position
+		mousePos.z = 0;
+		//apply
+		particleTrans.position = mousePos;
+	}
+}
